dbec797bb757b3971016ccd384847be35828d19e
Farok-Hossain <farokafs7@gmail.comm>
2022-04-16 15:18:38 +0600

    payment updated

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const admin = require("firebase-admin");
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
 
 const port = process.env.PORT || 5000;
 
@@ -52,11 +54,31 @@ async function run() {
             res.json(appointments);
         })
 
+        app.get('/appointments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await appointmentsCollection.findOne(query);
+            res.json(result);
+        })
+
         app.post('/appointments', async(req, res) => {
             const appointment = req.body;
             const result = await appointmentsCollection.insertOne(appointment);
             res.json(result)
         });
+
+        app.put('/appointment/:id', async(req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = {_id: ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                    payment: payment
+                }
+            };
+            const result = await appointmentsCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
 
         app.get('/users/:email', async(req, res) => {
             const email = req.params.email;
@@ -103,6 +125,17 @@ async function run() {
 
             
             
+        })
+
+        app.post('/create-payment-intent', async(req, res) => {
+            const paymentInfo = req.body;
+            const amount = paymentInfo.price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                payment_method_types: ['card']
+            });
+            res.json({ clientSecret: paymentIntent.client_secret })
         })
 
     }
